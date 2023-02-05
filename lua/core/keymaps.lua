fba85bed7652967997ed0383c3249eea22ffaafe
yzy <1933920397@qq.com>
2023-02-05 18:41:15 +0800

    修改窗口移动快捷建,修改保存文件和退出窗口快捷建

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -26,13 +26,16 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- 窗口
 keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
 keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set('n', '<leader>w', '<cmd>w!<CR>')
+keymap.set('n', '<leader>q', '<cmd>q!<CR>')
 
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
--- 切换buffer
-keymap.set("n", "<leader>l", ":bnext<CR>")
-keymap.set("n", "<leader>h", ":bprevious<CR>")
+
+-- -- 切换buffer
+-- keymap.set("n", "<leader>l", ":bnext<CR>")
+-- keymap.set("n", "<leader>h", ":bprevious<CR>")
 
 -- ---------- 插件 ---------- ---
 -- nvim-tree
@@ -46,3 +49,7 @@ vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
 vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+--BufferLine
+keymap.set('n', '<TAB>', ':BufferLineCycleNext <CR>') --移动窗口
+keymap.set('n', '<S-TAB>', ':BufferLineCyclePrev <CR>')
